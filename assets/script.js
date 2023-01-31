c013a57f8b4109894073dcb47558014599c15562
James Xiong <jxiong783@gmail.com>
2023-01-30 20:43:02 -0800

    added the start of a character creation for name/class and added snazzymaps onto the locations page

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,20 @@
-// script goes here
+
+  function createCharacter() {
+    // Get the character information from the form
+    const charName = document.getElementById("char-name").value;
+    const charClass = document.getElementById("char-class").value;
+
+    // Store the character information in local storage
+    localStorage.setItem("charName", charName);
+    localStorage.setItem("charClass", charClass);
+
+    // Display the character information on the page
+    const charInfo = document.getElementById("char-info");
+    charInfo.innerHTML = `Name: ${charName}<br>Class: ${charClass}`;
+  }
+
+  const form = document.querySelector("form");
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    createCharacter();
+  });
